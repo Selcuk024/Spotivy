2de298bd510ddfb622a69c877107b981852fd722
Selcuk <1202320@student.roc-nijmegen.nl>
2024-05-24 16:07:10 +0200

    changed user.cs

--- Playlist.cs
+++ Playlist.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Spotivy
 {
-    internal class Playlist
+    public class Playlist
     {
     }
 }
--- User.cs
+++ User.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Spotivy
 {
-    internal class User
+
+    public class User
     {
+        public List<Playlist> Playlists { get; set; }
+
+        public string Username;
+
+        Playlist playlist = new Playlist();
+
+        public List<Playlist> UserPlaylists = new List<Playlist>();
+        
+
+        public void AddPlaylist(Playlist playlist)
+        {
+            Playlists.Add(playlist);
+            Console.WriteLine($"Playlist '{playlist.Name}' added to user '{Username}'.");
+        }
+        public void 
+
+    }
     }
 }
