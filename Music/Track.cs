a097325e33c89e37c580d9295a5ea6b8654f50e6
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2024-05-24 16:05:17 +0200

    added music file, tracks and playlist

---
+++ Music/Track.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotivy.Music
+{
+    internal class Track
+    {
+        private int id;
+        private string name;
+        private string producer;
+        private int timeInSeconds;
+
+        public int getTrackId()
+        {
+            return id;
+        }
+        public string getTrackName ()
+        {
+            return name;
+        }
+        public string getTrackProducer ()
+        {
+            return producer;
+        }
+        public int getTrackTimeInSeconds()
+        {
+            return timeInSeconds;
+        }
+    }
+}
