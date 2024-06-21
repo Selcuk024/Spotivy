1c2d601fb8d1e35ea5dd087b69f4f47dafa4e12b
Selcuk <1202320@student.roc-nijmegen.nl>
2024-06-21 15:31:30 +0200

    changed track.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Spotivy
+﻿using Spotivy.Music;
+
+namespace Spotivy
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Track track = new Track(1, "test", "test");
+            List<Track> trackList = new List<Track>();
+            
+            bool playing = track.PlaySong();
+            string songName = track.getTrackName();
+            if (playing == true)
+            {
+                Console.WriteLine("song is playing");
+            }
             User user = new User();
             user.createUser("Daan");
             user.createUser("Selcuk");
